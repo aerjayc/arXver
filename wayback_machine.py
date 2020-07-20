14c40c8396c0f4e653bf1c7fc3d611d46e92376f
aerjayc <aerjay.castaneda@gmail.com>
2020-07-20 20:40:51 +0800

    Initial wayback machine code
    This literally just takes the json response of WB for the query,
    converts it to a `dict`, and returns it.

---
+++ wayback_machine.py
@@ -0,0 +1,37 @@
+import requests
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+from archive_today import validate_url
+
+
+def get_fast_wayback_machine(url,
+                             user_agent="arXver/0.3.0"):
+    """Return a dict of archive URLS and metadata."""
+
+    # validate url
+    assert validate_url(url), f'Invalid URL: "{url}"'
+
+    # get fast url
+    wayback_endpoint = 'http://web.archive.org/cdx/search/cdz'
+    params = {'url': url,
+              'fastLatest': True,
+              'output': 'json'
+              }
+
+    # create Session
+    # based on https://stackoverflow.com/a/35504626/11905538
+    sess = requests.Session()
+    retries = Retry(total=5,
+                    backoff_factor=0.1,
+                    status_forcelist=[500, 502, 503, 504])
+    sess.mount('http://', HTTPAdapter(max_retries=retries))
+    sess.mount('https://', HTTPAdapter(max_retries=retries))
+
+    get_kwargs = {'timeout': 30,
+                  'allow_redirects': True,
+                  'params': params,
+                  'headers': {'User-Agent': user_agent}
+                  }
+    response = sess.get(wayback_endpoint, **get_kwargs)
+
+    return response
