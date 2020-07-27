bb482b25a37542480a1f3295330fa627089c99f1
aerjayc <aerjay.castaneda@gmail.com>
2020-07-28 00:19:34 +0800

    Move general functions to own file

--- archive_today.py
+++ archive_today.py
@@ -1,5 +1,5 @@
-import re
 import requests
+from utils import validate_url, link_header_parser
 
 
 def get_first_archive_today(url,
@@ -43,34 +43,3 @@ def get_first_archive_today(url,
 
     return None
 
-
-def link_header_parser(string):
-    """Parse Link header field."""
-
-    exp = re.compile(r'<(\S+)>;\s*rel="([^"]+)",?\s*')
-
-    # if want to catch "'" users, use this:
-    # exp = re.compile(r'<(\S+)>;\s*rel=("([^"]+)"|'([^"]+)'),?\s*')
-
-    matches = re.findall(exp, string)
-
-    return dict((rel, url) for url, rel in matches)
-
-
-def validate_url(string):
-    """Return True if `string` is a valid url, else return False.
-
-    Based on https://stackoverflow.com/a/7160778/11905538
-    """
-
-    regex = re.compile(
-        r'^(?:http|ftp)s?://'   # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'
-        r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-        r'localhost|'           # localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-        r'(?::\d+)?'            # optional port
-        r'(?:/?|[/?]\S+)$',
-        re.IGNORECASE)
-
-    return re.match(regex, string) is not None
