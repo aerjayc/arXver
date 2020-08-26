d4afd0c3bc7311b804c0835512c616380a04287b
aerjayc <aerjay.castaneda@gmail.com>
2020-08-26 12:21:50 +0800

    Make raw url regex a variable

--- utils.py
+++ utils.py
@@ -1,5 +1,14 @@
 import re
 
+URL_RAW_REGEX = (r'^(?:http|ftp)s?://'     # http:// or https://
+                 r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'
+                 r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'    # domain...
+                 r'localhost|'            # localhost...
+                 r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'    # ...or ip
+                 r'(?::\d+)?'             # optional port
+                 r'(?:/?|[/?]\S+)$')
+
+
 
 def link_header_parser(string):
     """Parse Link header field."""
@@ -20,15 +29,7 @@ def validate_url(string):
     Based on https://stackoverflow.com/a/7160778/11905538
     """
 
-    regex = re.compile(
-        r'^(?:http|ftp)s?://'   # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'
-        r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-        r'localhost|'           # localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-        r'(?::\d+)?'            # optional port
-        r'(?:/?|[/?]\S+)$',
-        re.IGNORECASE)
+    regex = re.compile(URL_RAW_REGEX, re.IGNORECASE)
 
     return re.match(regex, string) is not None
 
