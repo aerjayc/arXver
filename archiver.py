fa786caf724296323636e943b204213ebcad4296
aerjayc <aerjay.castaneda@gmail.com>
2020-12-29 14:33:08 +0800

    Use stdin if no input file
    The script can now use pipe inputs instead of always needing the input
    urls in a text file. The parsing algorithm uses regex, so it should be
    able to parse all urls in whatever format (csv, space-separated, one url
    per line, etc.). Note that this is true for the file input as well.
    
    Consequently, the file input is now an optional argument (`-i`), which
    overrides stdin if used.
    
    Variable names were also changed. E.g.:
    - `args.save`   -> `args.output_dir`
    - `save_dir`    -> `output_dir`
    - `metavar=DIR` -> `metavar=DIRECTORY`

--- archiver.py
+++ archiver.py
@@ -4,13 +4,14 @@ import os.path
 from pathlib import Path
 import time
 import argparse
+import sys
 
 
-def submit_unarchived(urls, save_dir=None, overwrite=False, force=False,
+def submit_unarchived(urls, output_dir=None, overwrite=False, force=False,
                       query_pause=2, submit_pause=5, user_agent=user_agent):
 
-    if save_dir is not None:
-        Path(save_dir).mkdir(parents=True, exist_ok=True)
+    if output_dir is not None:
+        Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     archived_urls = dict()
     for i, url in enumerate(urls):
@@ -31,9 +32,9 @@ def submit_unarchived(urls, save_dir=None, overwrite=False, force=False,
         if i < (len(urls) - 1):     # don't sleep after the last url
             time.sleep(submit_pause)
 
-        if save_dir is not None:
+        if output_dir is not None:
             filename = utils.url_to_filename(url, extension='.html')
-            filepath = os.path.join(save_dir, filename)
+            filepath = os.path.join(output_dir, filename)
 
             if os.path.isfile(filepath) and not overwrite:
                 print('\tSkipping', filename, '(file exists)...')
@@ -47,24 +48,32 @@ def submit_unarchived(urls, save_dir=None, overwrite=False, force=False,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('urls_file', help='file containing urls to archive')
-    parser.add_argument('-s', '--save', metavar='DIR',
-                        help='where to save the raw htmls')
+    parser.add_argument('-i', '--input', metavar='FILE', dest='input',
+                        help='file containing urls to archive (overrides pipe input)')
+    parser.add_argument('-o', '--output', metavar='DIRECTORY',
+                        help='output directory for the raw htmls')
     parser.add_argument('-f', '--force', action='store_true',
                         help='submit url even if already archived')
     parser.add_argument('-w', '--overwrite', action='store_true',
                         help='overwrite file if it exists')
     parser.add_argument('-t', '--time_delay', metavar='SECS', default=5, type=int,
-                        help='time delay between archive submissions')
+                        help='time delay between archive submissions (default is 5)')
     parser.add_argument('-u', '--user_agent')
     parser.add_argument('--spoof', action='store_true',  help='use a common user agent')
     args = parser.parse_args()
 
+    if args.input:
+        urls = utils.extract_urls_from_file(args.input)
+    elif not sys.stdin.isatty():
+        urls = utils.extract_urls(sys.stdin.read())
+    else:
+        parser.print_usage()
+        exit()
+
     if args.spoof:
         user_agent = common_user_agent
-    urls = utils.extract_urls(args.urls_file)
 
-    submit_unarchived(urls, save_dir=args.save, submit_pause=args.time_delay,
+    submit_unarchived(urls, output_dir=args.output, submit_pause=args.time_delay,
                       force=args.force, overwrite=args.overwrite,
                       user_agent=user_agent)
 
