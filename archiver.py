71cfff297702b69019cccea647bdbdc3f06e679b
aerjayc <aerjay.castaneda@gmail.com>
2020-12-29 12:30:57 +0800

    Allow to force submission even if already archived
    Change `-f` or `--force` flag to mean "submit even if already archived". The
    overwrite flag is now `--overwrite` or `-w`. Add metavar to `--save`.
    Change location of `time.sleep` so we see feedback before the script
    pauses.

--- archiver.py
+++ archiver.py
@@ -6,8 +6,8 @@ import time
 import argparse
 
 
-def submit_unarchived(urls, save_dir=None, overwrite=False, query_pause=2,
-                      submit_pause=5, user_agent=user_agent):
+def submit_unarchived(urls, save_dir=None, overwrite=False, force=False,
+                      query_pause=2, submit_pause=5, user_agent=user_agent):
 
     if save_dir is not None:
         print(save_dir, 'does not exist. Creating directory...')
@@ -22,12 +22,14 @@ def submit_unarchived(urls, save_dir=None, overwrite=False, query_pause=2,
         if results:
             archived_urls[url] = results[0][0]
             print('Already archived at', archived_urls[url])
-            continue
+
+            if not force:
+                continue
 
         response = wayback_machine.submit_wayback(url, user_agent=user_agent)
-        time.sleep(submit_pause)
         archived_urls[url] = response.url
         print('Submitted to', response.url)
+        time.sleep(submit_pause)
 
         if save_dir is not None:
             filename = utils.url_to_filename(url, extension='.html')
@@ -46,8 +48,12 @@ def submit_unarchived(urls, save_dir=None, overwrite=False, query_pause=2,
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('urls_file', help='file containing urls to archive')
-    parser.add_argument('-s', '--save', help='where to save the raw htmls')
-    parser.add_argument('-f', '--force', help='overwrite file if it exists')
+    parser.add_argument('-s', '--save', metavar='DIR',
+                        help='where to save the raw htmls')
+    parser.add_argument('-f', '--force', action='store_true',
+                        help='submit url even if already archived')
+    parser.add_argument('-w', '--overwrite', action='store_true',
+                        help='overwrite file if it exists')
     parser.add_argument('-u', '--user_agent')
     parser.add_argument('--spoof', action='store_true',  help='use a common user agent')
     args = parser.parse_args()
@@ -56,5 +62,6 @@ if __name__ == '__main__':
         user_agent = common_user_agent
     urls = utils.extract_urls(args.urls_file)
 
-    submit_unarchived(urls, save_dir=args.save, overwrite=args.force, user_agent=user_agent)
+    submit_unarchived(urls, save_dir=args.save, force=args.force,
+                      overwrite=args.overwrite, user_agent=user_agent)
 
